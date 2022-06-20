193a6eaff8d45726366bcd885d5db60b151ad5b9
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-06-19 20:38:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 status = "";
-object = "";
+objects = [];
 
-function setup() {
-    canvas = createCanvas(400, 400);
+function setup(){
+    canvas = createCanvas(400,400);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(400, 400);
+    video.size(400,400);
     video.hide();
 }
 
-function start() {
-    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    document.getElementById("object").value = object;
+    value = document.getElementById("object").value;
 }
 
-function modelloaded() {
+function modelLoaded(){
     console.log("Model Loaded!");
     status = true;
 }
 
 function draw(){
-    image(video, 0, 0, 380, 380);
+    image(video, 0, 0, 400,400);
+    if(status != ""){
+        for(i = 0; i< objects.length; i++){
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(results){
+    if(error){
+        console.error;
+    }
+    console.log(results);
+    objects = results;
 }
